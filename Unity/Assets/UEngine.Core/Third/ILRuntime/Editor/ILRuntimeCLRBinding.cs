a320cde2b64f20947595cd13aa8142f296ce04db
mouserices <mouse.rices@gmail.com>
2022-02-15 18:06:04 +0800

    将ILruntime Editor相关脚本放在Thid/ILRuntime下

---
+++ Unity/Assets/UEngine.Core/Third/ILRuntime/Editor/ILRuntimeCLRBinding.cs
@@ -0,0 +1,47 @@
+﻿#if UNITY_EDITOR
+using System.Reflection;
+using Entitas;
+using UEngine;
+using UnityEditor;
+using UnityEngine;
+
+[System.Reflection.Obfuscation(Exclude = true)]
+public class ILRuntimeCLRBinding
+{
+   [MenuItem("ILRuntime/通过自动分析热更DLL生成CLR绑定")]
+    static void GenerateCLRBindingByAnalysis()
+    {
+        //用新的分析热更dll调用引用来生成绑定代码
+        ILRuntime.Runtime.Enviorment.AppDomain domain = new ILRuntime.Runtime.Enviorment.AppDomain();
+        using (System.IO.FileStream fs = new System.IO.FileStream("Assets/StreamingAssets/UEngine.HotFix.dll", System.IO.FileMode.Open, System.IO.FileAccess.Read))
+        {
+            domain.LoadAssembly(fs);
+        
+            //Crossbind Adapter is needed to generate the correct binding code
+            InitILRuntime(domain);
+            ILRuntime.Runtime.CLRBinding.BindingCodeGenerator.GenerateBindingCode(domain, "Assets/UEngine.Core/Generated");
+        }
+        
+        AssetDatabase.Refresh();
+    }
+
+    static void InitILRuntime(ILRuntime.Runtime.Enviorment.AppDomain domain)
+    {
+        //这里需要注册所有热更DLL中用到的跨域继承Adapter，否则无法正确抓取引用
+        // domain.RegisterCrossBindingAdaptor(new MonoBehaviourAdapter());
+        // domain.RegisterCrossBindingAdaptor(new CoroutineAdapter());
+        // domain.RegisterCrossBindingAdaptor(new TestClassBaseAdapter());
+        // domain.RegisterValueTypeBinder(typeof(Vector3), new Vector3Binder());
+        // domain.RegisterValueTypeBinder(typeof(Vector2), new Vector2Binder());
+        // domain.RegisterValueTypeBinder(typeof(Quaternion), new QuaternionBinder());
+        
+        domain.RegisterCrossBindingAdaptor(new EntityAdapter());
+        domain.RegisterCrossBindingAdaptor(new ContextAttributeAdaptor());
+        domain.RegisterCrossBindingAdaptor(new ContextAdapter());
+        domain.RegisterCrossBindingAdaptor(new SystemsAdaptor());
+        domain.RegisterCrossBindingAdaptor(new ReactiveSystemAdapter());
+        domain.RegisterCrossBindingAdaptor(new IContextsAdapter());
+        domain.RegisterCrossBindingAdaptor(new IComponentAdapter());
+    }
+}
+#endif
